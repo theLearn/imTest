8177ce830d0e0bdfb18179a746c71f2639ce111b
洪诚 <18502729006@163.com>
2019-03-26 11:01:15 +0800

    Update GroupMemberListActivity.kt

--- app/src/main/java/com/project/hc/imtest/activity/GroupMemberListActivity.kt
+++ app/src/main/java/com/project/hc/imtest/activity/GroupMemberListActivity.kt
@@ -4,18 +4,15 @@ import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.OrientationHelper
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
-import com.example.hongcheng.common.base.BaseListAdapter
 import com.example.hongcheng.common.util.ToastUtils
 import com.example.hongcheng.data.retrofit.ActionException
 import com.example.hongcheng.data.retrofit.BaseSubscriber
 import com.example.hongcheng.data.retrofit.RetrofitClient
 import com.example.hongcheng.data.retrofit.RetrofitManager
-import com.hyphenate.chat.EMConversation
 import com.project.hc.imtest.R
 import com.project.hc.imtest.adapter.GroupMemberAdapter
 import com.project.hc.imtest.api.ApiRetrofit
 import com.project.hc.imtest.application.BaseApplication
-import com.project.hc.imtest.chat.ChatUtils
 import com.project.hc.imtest.model.GroupInfo
 import com.project.hc.imtest.model.GroupMemberInfo
 import com.project.hc.imtest.model.GroupMemberInfoList
@@ -23,7 +20,7 @@ import kotlinx.android.synthetic.main.body_group_member_list.*
 import kotlinx.android.synthetic.main.layout_app_common_title.*
 
 
-class GroupMemberListActivity : AppCommonActivity(), BaseListAdapter.OnItemClickListener {
+class GroupMemberListActivity : AppCommonActivity() {
 
     private lateinit var mAdapter: GroupMemberAdapter
     private var dataList : MutableList<GroupMemberInfo> = arrayListOf()
@@ -48,7 +45,6 @@ class GroupMemberListActivity : AppCommonActivity(), BaseListAdapter.OnItemClick
         rv_group_member_list.layoutManager = StaggeredGridLayoutManager(5, OrientationHelper.VERTICAL)
         rv_group_member_list.itemAnimator = DefaultItemAnimator()
         mAdapter = GroupMemberAdapter()
-        mAdapter.onItemClickListener = this
         rv_group_member_list.adapter = mAdapter
         getData()
     }
@@ -74,12 +70,5 @@ class GroupMemberListActivity : AppCommonActivity(), BaseListAdapter.OnItemClick
                     }
                 }))
 
-    }
-
-    override fun onItemClick(position: Int) {
-        ChatUtils.goToChat(this, mAdapter.getItem(position).mid, "", EMConversation.EMConversationType.Chat)
-    }
-
-    override fun onItemLongClick(position: Int) {
     }
 }
